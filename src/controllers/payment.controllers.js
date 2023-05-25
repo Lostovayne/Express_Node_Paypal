d7156c3fe21b1f34d0b213f65cec5ed86ee6f0c5
Losto <92962731+Lostovayne@users.noreply.github.com>
2023-05-25 18:08:42 -0400

    fixed

--- src/controllers/payment.controllers.js
+++ src/controllers/payment.controllers.js
@@ -1,21 +1,21 @@
 export const createOrder = (req, res) => {
-  const order = {
-    intent: "CAPTURE",
-    purchase_units: [
-      {
-        amount: {
-          value: "100",
-          currency_code: "USD",
-        },
-      },
-    ],
-  };
-};
+    const order = {
+        intent: 'CAPTURE',
+        purchase_units: [
+            {
+                amount: {
+                    value: '100',
+                    currency_code: 'USD'
+                }
+            }
+        ]
+    }
+}
 
 export const captureOrder = (req, res) => {
-  res.send("capture order");
-};
+    res.send('capture order')
+}
 
 export const cancelPayment = (req, res) => {
-  res.send("cancel payment");
-};
+    res.send('cancel payment')
+}
